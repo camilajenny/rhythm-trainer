a34aadea789759d0ec8b985847aa226c0a35bb55
Kamila Łopuszańska <kamil.lopuszanski@gmail.com>
2025-07-11 19:14:37 +0200

    create the Exercise model

---
+++ app/Models/Exercise.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use App\Transformers\BaseTransformer;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Exercise extends BaseModel
+{
+    use HasFactory;
+
+    /**
+     * @var string UUID key of the resource
+     */
+    public $primaryKey = '_id';
+
+    /**
+     * @var null|array What relations should one model of this entity be returned with, from a relevant controller
+     */
+    public static ?array $itemWith = [];
+
+    /**
+     * @var null|array What relations should a collection of models of this entity be returned with, from a relevant controller
+     * If left null, then $itemWith will be used
+     */
+    public static ?array $collectionWith = null;
+
+    /**
+     * @var null|BaseTransformer The transformer to use for this model, if overriding the default
+     */
+    public static $transformer = null;
+
+    /**
+     * @var array The attributes that are mass assignable.
+     */
+    protected $fillable = ['name', 'description', 'difficulty', 'bpm_override'];
+
+    /**
+     * @var array The attributes that should be hidden for arrays and API output
+     */
+    protected $hidden = [];
+
+    /**
+     * Return the validation rules for this model
+     *
+     * @return array Rules
+     */
+    public function getValidationRules(): array
+    {
+        return [];
+    }
+
+}
---
+++ database/migrations/2025_07_11_082514_create_exercises_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exercises', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('rhythm_pattern_id')->constrained('rhythm_patterns')->onDelete('cascade');
+            $table->string('name');
+            $table->string('description');
+            $table->string('difficulty');
+            $table->integer('bpm_override');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exercises');
+    }
+};
